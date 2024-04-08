ba659b5a503161489aadecfbfbecbd4472015bc6
weihong00827 <1176101021qiu@gmail.com>
2024-04-08 06:16:53 +0000

    question1

--- student_files/student_files/q1.py
+++ student_files/student_files/q1.py
@@ -1,11 +1,34 @@
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import size
+from pyspark import SparkContext, SparkConf
 # you may add more import if you need to
 
 
 # don't change this line
 hdfs_nn = sys.argv[1]
 
-spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
+conf = SparkConf().setAppName("Wordcount Application")
+sc = SparkContext(conf=conf)
+spark = SparkSession(sc)
 # YOUR CODE GOES BELOW
 
+df = spark.read.option("header", True).csv(
+    "hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn)
+)
+df.printSchema()
+original_count = df.count()
+rating_filter = df.filter(df.Rating > 1)
+full_filter = rating_filter.filter(df.Reviews.isNotNull())
+
+full_filtered_count = full_filter.count()
+# df.filter(df.Rating < 1).show()
+# rating_count = rating_filter.count()
+# review_filter = df.filter(df.Reviews.isNotNull())
+# review_count = review_filter.count()
+# print(
+#     f"original_count:{original_count}, rating_count{rating_count},review_count:{review_count}, full filter count:{full_filtered_count}"
+# )
+
+
+full_filter.write.format("csv").save("/assignment2/output/question1/")
